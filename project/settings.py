fb714d2487f8d7d76a02121a5ce563e1d61b69c7
bouzenaali <Alibouzena16@gmail.com>
2023-12-29 15:32:54 +0100

    setup the project

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'conversation',
+    'core',
+    'dashboard',
+    'item',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
