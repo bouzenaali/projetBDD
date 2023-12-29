a2c5b389bd177384a272a3e4a7d2e495278f8361
bouzenaali <Alibouzena16@gmail.com>
2023-12-29 17:34:06 +0100

    chore: edit wsgi.py to chose the settings.py in local use and deployment.py when in deployment

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
