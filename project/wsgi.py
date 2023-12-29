e34e9d1dd2010127f662e1badf8f17e6bc03ad2f
bouzenaali <Alibouzena16@gmail.com>
2023-12-29 19:00:38 +0100

    chore: correct the project name spelling in the wsgi.py file again

--- project/wsgi.py
+++ project/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'projetBDD.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'projetBDD.settings'
+settings_module = 'project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'project.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
