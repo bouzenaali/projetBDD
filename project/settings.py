ca73f50cf045ebfff58c5ef8c25676a97049506b
bouzenaali <Alibouzena16@gmail.com>
2024-01-26 11:09:30 +0100

    chore: migrate sqlite into mysql

--- project/settings.py
+++ project/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "vintage",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "USER": "root",
+        "PASSWORD": "",
     }
 }
 
